33025af1e2a04a4d79a06e7b3355d79745076f54
xiaoding <2239058652@qq.com>
2026-03-04 15:40:49 +0800

    feat: 初始化EFoodPass Spring Boot项目

---
+++ src/main/java/com/epass/food/EFoodPassApplication.java
@@ -0,0 +1,13 @@
+package com.epass.food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EFoodPassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EFoodPassApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/epass/food/EFoodPassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epass.food;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EFoodPassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
